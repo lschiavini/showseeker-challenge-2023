b1c99d73f221585c2036865fafc5a3014a085fbc
Lucas Schiavini <lucas.s.schiavini@gmail.com>
2023-07-13 13:18:49 -0300

    init anti day tests

---
+++ tests/anti-day.test.ts
@@ -0,0 +1,12 @@
+
+import { executeRules } from '../src/index';
+
+describe('test sad paths', () => {
+    it('mxxx is Monday and should return error', () => {
+        const result = executeRules('mxxx')
+        const correctResult = 'invalid input - names of the days must contain the letters of the days of the week'
+        expect(result).toBe(correctResult)
+    })
+
+})
+
